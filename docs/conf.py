4936e3c1433f32b487a0e5bf6192403ed5032d65
Andrey <and-lukyane@yandex.ru>
2020-09-07 18:15:38 +0300

    Trying to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,8 @@ import datetime
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../src/'))
 
 # -- Project information -----------------------------------------------------
 
