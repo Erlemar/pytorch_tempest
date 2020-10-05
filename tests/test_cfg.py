9aa08e16401048a5c339fac6393a68a15738a454
Andrey <and-lukyane@yandex.ru>
2020-10-05 05:31:33 +0300

    fix tests

--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -9,8 +9,7 @@ config_files = [f.split('.')[0] for f in os.listdir(path) if 'yaml' in f]
 
 
 @pytest.mark.parametrize('config_name', config_files)
-def test_cfg(config_name: str) -> None:
-    path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/conf'
-    with initialize_config_dir(config_dir=path):
+def test_cfg(config_name: str, config_path: str = path) -> None:
+    with initialize_config_dir(config_dir=config_path):
         cfg = compose(config_name=config_name)
         assert isinstance(cfg, DictConfig)
--- tests/test_schedulers.py
+++ tests/test_schedulers.py
@@ -10,9 +10,9 @@ path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/conf'
 
 
 @pytest.mark.parametrize('sch_name', os.listdir(path + '/scheduler'))
-def test_schedulers(sch_name: str, path: str = path) -> None:
+def test_schedulers(sch_name: str, config_path: str = path) -> None:
     scheduler_name = sch_name.split('.')[0]
-    with initialize_config_dir(config_dir=path):
+    with initialize_config_dir(config_dir=config_path):
         cfg = compose(config_name='config', overrides=[f'scheduler={scheduler_name}', 'optimizer=sgd'])
         optimizer = load_obj(cfg.optimizer.class_name)(torch.nn.Linear(1, 1).parameters(), **cfg.optimizer.params)
         load_obj(cfg.scheduler.class_name)(optimizer, **cfg.scheduler.params)
